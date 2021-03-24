a7d0415c6a636a21977c3e4e83d1b34cfe903222
Jonathan Galdino <60003503@midway.com.br>
2021-03-24 11:01:36 -0300

    moving folders to goexamples

--- goexamples/slices/main.go
+++ goexamples/slices/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	s := make([]string, 3)
-	fmt.Println("emp: ", s)
+	fmt.Println("emp:", s)
 
 	s[0] = "a"
 	s[1] = "b"
@@ -13,14 +13,11 @@ func main() {
 	fmt.Println("set:", s)
 	fmt.Println("get:", s[2])
 
-	fmt.Println("len: ", len(s))
+	fmt.Println("len:", len(s))
 
 	s = append(s, "d")
 	s = append(s, "e", "f")
 	fmt.Println("apd:", s)
-	t := append(s, "gg")
-	fmt.Println("apd: ", t)
-	fmt.Println("len: ", len(s))
 
 	c := make([]string, len(s))
 	copy(c, s)
@@ -28,4 +25,23 @@ func main() {
 
 	l := s[2:5]
 	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
