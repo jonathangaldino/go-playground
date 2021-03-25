c2dcbc2d69e45a3000cdadbd2aa7cd1f79096da5
Jonathan Galdino <60003503@midway.com.br>
2021-03-25 10:34:17 -0300

    Validating

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Page struct {
@@ -19,6 +21,20 @@ func (p *Page) save() error {
 
 // Caches templates to avoid use template.ParseFiles more than once
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+// Regexp to validate path
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+
+	return m[2], nil // the title is the second expression
+}
 
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
@@ -39,10 +55,15 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, error := loadPage(title)
+	title, err := getTitle(w, r)
 
-	if error != nil {
+	if err != nil {
+		return
+	}
+
+	p, err := loadPage(title)
+
+	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusNotFound)
 		return
 	}
@@ -51,7 +72,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -62,11 +88,16 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	body := r.FormValue("body")
 
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
